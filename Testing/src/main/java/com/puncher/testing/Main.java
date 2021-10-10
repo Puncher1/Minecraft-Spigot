c25c4e8903a275d887b93e624dc5c6bcea64d946
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-10-10 15:33:32 +0200

    Renamed main

--- Testing/src/main/java/com/puncher/testing/Main.java
+++ Testing/src/main/java/com/puncher/testing/Main.java
@@ -2,7 +2,7 @@ package com.puncher.testing;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Testing extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
