8d58d4bf11154451d1ea8911bfcc9a2d79a9ec05
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-10-10 15:21:39 +0200

    Created project

---
+++ Testing/src/main/java/com/puncher/testing/Testing.java
@@ -0,0 +1,17 @@
+package com.puncher.testing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Testing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
