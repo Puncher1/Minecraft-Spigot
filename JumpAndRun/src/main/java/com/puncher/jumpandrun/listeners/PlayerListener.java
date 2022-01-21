d83d92e5fc89ac9409f73cf95d460b07bf720c67
Puncher <mcocexpert@hispeed.ch>
2022-01-21 13:01:31 +0100

    Spawn block and remove block when falling down

--- JumpAndRun/src/main/java/com/puncher/jumpandrun/listeners/PlayerListener.java
+++ JumpAndRun/src/main/java/com/puncher/jumpandrun/listeners/PlayerListener.java
@@ -11,14 +11,16 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerListener implements Listener {
 
+    private static boolean isJumpAndRunRunning = false;
+
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event)
     {
         Player player = event.getPlayer();
 
-        if (event.isCancelled() ||
-            event.getFrom().getBlock().getLocation() == event.getFrom().getBlock().getLocation())
+        if (event.isCancelled())            // TODO: Checking if block has changed (otherwise ignore)
         {
+            System.out.println("return");
             return;
         }
 
@@ -27,13 +29,33 @@ public class PlayerListener implements Listener {
         double player_y = playerLocation.getY();
         double player_z = playerLocation.getZ();
         Block playerBlock = playerLocation.getBlock();
-        Location targetLocation = new Location(player.getWorld(), 124, 76, -90);
+        Location currentBlockLocation = new Location(player.getWorld(), 124.5, 80, -89.5);
+        Location nextBlockLocation = new Location(player.getWorld(), 127.5, 81, -89.5);
 
         if ((player_x > 124 && player_x < 125) && (player_y == 76) && (player_z > -90 && player_z < -89)
                 && playerBlock.getType() == Material.OAK_PRESSURE_PLATE)
         {
-            Location newLocation = new Location(player.getWorld(), 124.5, 80, -89.5, +90, 0);
-            player.teleport(newLocation);
+            Location newPlayerLocation = new Location(player.getWorld(), 124.5, 81, -89.5, -90, 0);
+            player.teleport(newPlayerLocation);
+            currentBlockLocation.getBlock().setType(Material.LIGHT_BLUE_TERRACOTTA);
+            nextBlockLocation.getBlock().setType(Material.CYAN_WOOL);
+
+            playerLocation = player.getLocation();
+            player_y = playerLocation.getY();
+
+            isJumpAndRunRunning = true;
+        }
+
+        if (isJumpAndRunRunning)
+        {
+            System.out.println((int)player_y);
+            System.out.println((int)currentBlockLocation.getY());
+            if ((int)player_y < (int)currentBlockLocation.getY())
+            {
+                isJumpAndRunRunning = false;
+                currentBlockLocation.getBlock().setType(Material.AIR);
+                nextBlockLocation.getBlock().setType(Material.AIR);
+            }
         }
 
 
